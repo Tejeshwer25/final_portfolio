ae0dbe48e003de08df61d4a7e8efc67b09afa4fb
Tejeshwer Singh Sachdeva <57140381+Tejeshwer25@users.noreply.github.com>
2023-06-08 12:11:53 +0530

    Update Data.js

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -5,7 +5,7 @@ export const data = {
     profession: "Front-End Developer",
     bio: [
       "Hello, I'am Tejeshwer Singh Sachdeva!!!👳‍♂️",
-      "I am a 20yr old Front-End Developer based in Jhansi, India. I am currently pursuing my Bachelors in Computer Science and Engineering from ITM University, Gwalior. Apart from that I am an all time explorer in the field of web & app development👨‍💻.",
+      "I am a 23yr old Front-End Developer based in Jhansi, India. I am currently pursuing my Bachelors in Computer Science and Engineering from ITM University, Gwalior. Apart from that I am an all time explorer in the field of web & app development👨‍💻.",
       "'All work and no play makes jack a dull boy', this is a famous a quote and one of the main reasons I love playing games mostly outdoor. My hobbies include: Indulging with Music🎼, Sports(like Cricket🏏, Football⚽, Badminton🏸, etc), Little bit of Reading📖...",
       "Apart from the Tech stuff I am also a Defence Aspirant and crazy about the Indian Armed Forces👮‍♂️.",
     ],
