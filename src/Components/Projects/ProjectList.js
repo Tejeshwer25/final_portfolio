32db86592faf538d05c4d363dd3ca97e0188a2f2
Tejeshwer Singh <tejeshwersingh2504@gmail.com>
2025-01-07 23:25:31 +0530

    Show see more only if featured projects exceed 3

--- src/Components/Projects/ProjectList.js
+++ src/Components/Projects/ProjectList.js
@@ -10,6 +10,8 @@ function ProjectList(props) {
         if(pinProjects.length >=3) {
             break;
         }
+
+        console.log(props);
     }    
 
     return (
@@ -18,7 +20,10 @@ function ProjectList(props) {
                 <ProjectCard data={item} />
             )}  
 
-            <ProjectCard data={undefined} title={props.title}/>
+            {
+                // Show below component only if list length is greater than 4, props.data is an object
+                (Object.keys(props.data).length) > 3 && <ProjectCard data={undefined} title={props.title}/>
+            }   
         </div>
     )
 }
